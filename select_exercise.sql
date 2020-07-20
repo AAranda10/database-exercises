69aece17965112c6ae833319424a26736cd0ed25
Austin Aranda <austinaranda10@gmail.com>
2020-07-20 16:02:44 -0500

    Select Exercise

---
+++ select_exercise.sql
@@ -0,0 +1,24 @@
+# 1
+select name from albums
+where artist = 'Pink Floyd';
+
+#2
+select release_date from albums
+where name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+
+#3
+select genre from albums
+where name = 'Nevermind';
+
+#4
+select name from albums
+where release_date between 1990 and 1999;
+
+#5
+select name from albums
+where sales > 20;
+
+#6
+select name from albums
+where genre = 'rock';
+#It doesn't include variations of Rock because equals means exactly the Rock verbage
