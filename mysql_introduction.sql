a7ca6427dff22767e06acb3e650f408fc06d28dc
Austin Aranda <austinaranda10@gmail.com>
2020-07-20 14:37:43 -0500

    final answers

--- mysql_introduction.sql
+++ mysql_introduction.sql
@@ -24,14 +24,4 @@ show create database employees;
 #7. all but salaries has a string type
 #8. all but departments and employees_with_departments
 #9. dept_emp and dept_manager form the relationship between both of those tables.
-#10.
-CREATE TABLE `dept_manager` (
-  `emp_no` int(11) NOT NULL,
-  `dept_no` char(4) NOT NULL,
-  `from_date` date NOT NULL,
-  `to_date` date NOT NULL,
-  PRIMARY KEY (`emp_no`,`dept_no`),
-  KEY `dept_no` (`dept_no`),
-  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
-  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+#10.show create table dept_manager;
