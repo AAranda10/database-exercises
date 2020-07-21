d559a91466ab6a3f9d33088fe13a2e939c474541
Austin Aranda <austinaranda10@gmail.com>
2020-07-21 14:13:25 -0500

    forgot to add 1 question

--- where_exercises.sql
+++ where_exercises.sql
@@ -38,7 +38,8 @@ where birth_date like '%%%%-12-25';
 
 #5-2
 select first_name, last_name, birth_date from employees
-where birth_date like '%%%%-12-25';
+where birth_date like '%%%%-12-25'
+and hire_date like '199%';
 
 #6
 select first_name, last_name from employees
