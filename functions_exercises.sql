d4b21e5233d220bce80e4071094edb6a3b36d03d
Austin Aranda <austinaranda10@gmail.com>
2020-07-22 10:25:03 -0500

    still working on 6

---
+++ functions_exercises.sql
@@ -0,0 +1,40 @@
+use employees;
+
+#2a
+select concat(first_name,' ',last_name) as full_name from employees
+where last_name like 'E%';
+
+#2b
+select concat(first_name,' ',last_name) as full_name from employees
+where last_name like 'E%'
+or last_name like '%e';
+
+#2c
+select concat(first_name,' ',last_name) as full_name from employees
+where last_name like 'E%'
+and last_name like '%e';
+
+#3a
+select upper(concat(first_name,' ',last_name)) as full_name from employees
+where last_name like 'E%';
+
+#3b
+select upper(concat(first_name,' ',last_name)) as full_name from employees
+where last_name like 'E%'
+or last_name like '%e';
+
+#3c
+select upper(concat(first_name,' ',last_name)) as full_name from employees
+where last_name like 'E%'
+and last_name like '%e';
+
+#4
+select first_name, last_name, hire_date, datediff(now(), hire_date) from employees
+where birth_date like '%%%%-12-25'
+and hire_date like '199%';
+
+#5a
+select max(salary) from salaries;
+
+#5b
+select min(salary) from salaries;
