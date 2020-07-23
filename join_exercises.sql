a4eeeebc9464fcf0104709e868222bafe462b52b
Austin Aranda <austinaranda10@gmail.com>
2020-07-23 17:09:09 -0500

    up to number 7

--- join_exercises.sql
+++ join_exercises.sql
@@ -27,3 +27,29 @@ on d.dept_no = dpt.dept_no
 where t.to_date > curdate() and dpt.dept_no = 'd009'
 group by title;
 
+#5
+SELECT concat(first_name, '_', last_name) as Department_Manager, d.dept_name, s.salary FROM employees AS e
+JOIN dept_manager AS dm
+  ON dm.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = dm.dept_no
+join salaries as s
+on s.emp_no = dm.emp_no
+  where dm.to_date > curdate() and s.to_date > curdate()
+  order by dept_name asc;
+
+  #6
+select d.dept_no, d.dept_name, count(de.dept_no) as num_employees from dept_emp as de
+join departments as d
+on de.dept_no = d.dept_no
+where de.to_date > curdate()
+group by d.dept_no;
+
+#7
+select d.dept_name, avg(s.salary) from salaries as s
+join dept_emp as dpt
+on s.emp_no = dpt.emp_no
+join departments as d
+on d.dept_no = dpt.dept_no
+where s.to_date > curdate()
+group by d.dept_name;
